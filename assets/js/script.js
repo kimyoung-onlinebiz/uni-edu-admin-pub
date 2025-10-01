0cafba16946999d482aa03f8e724ee41a3810c37
nade2028 <nade2028@kimyoung.co.kr>
2025-10-01 09:04:07 +0900

    배너관리 추가했습니다.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const footer = document.querySelector('.admin_footer');
     const searchSection = document.querySelector('.search_section');
     const btnOpenClose = document.querySelector('.btn_group .btn_openClose');
+    const layerWrapper = document.querySelector('.layer_wrapper');
+    const bannerListLayer = document.querySelector('.banner_list');
+    const bannerPositionLayer = document.querySelector('.banner_position');
 
     // 사이드바 토글
     if (toggleButton && adminWrapper && adminSidebar && adminContent && footer) {
@@ -24,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (searchSection) {
         searchSection.style.height = searchSection.offsetHeight + 'px';
     }
-
+ 
     // 검색영역 토글
     if (btnOpenClose && searchSection) {
         btnOpenClose.addEventListener('click', () => {
@@ -43,4 +46,26 @@ document.addEventListener('DOMContentLoaded', () => {
             searchSection.style.height = isSmall ? '' : searchSection.offsetHeight + 'px';
         });
     }
+
+    // 배너 관리 배너 목록 버튼 클릭 시 배너 목록 레이어 활성화
+    document.querySelector('.btn_bannerList .btn').addEventListener('click', () => {
+        bannerListLayer.classList.add('active');
+    });
+
+    // 배너 위치 안내 버튼 클릭 시 배너 위치 레이어 활성화
+    document.querySelector('.btn_bannerPosition').addEventListener('click', () => {
+        bannerPositionLayer.classList.add('active');
+    });
+
+    // 닫기 버튼(.btn_layerClose) 또는 확인 버튼(.layer_btnSection button) 클릭 시 모든 레이어 비활성화
+    document.addEventListener('click', function(e) {
+        if (
+            e.target.classList.contains('btn_layerClose') ||
+            (e.target.closest('.layer_btnSection') && e.target.classList.contains('btn'))
+        ) {
+            const layer = e.target.closest('.layer_wrapper');
+            if (layer) layer.classList.remove('active');
+        }
+    });
+
 });
